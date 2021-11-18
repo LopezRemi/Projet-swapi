07229c4113cdaec939534661ab4d11386f4843d2
Ryuu76000 <r.lopez76000@gmail.com>
2021-11-18 13:19:37 +0100

    recherche ok

--- src/Component/Recherche.js
+++ src/Component/Recherche.js
@@ -1,14 +1,87 @@
+import React, { useState, useEffect } from 'react';
+
 
 
 function Recherche() {
-    return (
-      
-        
-          <h2>Page de Recherches</h2>
-      
-      
-    );
-  }
+      const [error, setError] = useState(null);
+      const [isLoaded, setIsLoaded] = useState(false);
+      const [items, setItems] = useState([]);
+      const [input, setInput] = useState('1');
 
 
-  export default Recherche;
+      useEffect(() => {
+            console.log(input)
+            fetch(`https://swapi.dev/api/people/${input}/`)
+                  .then(res => res.json())
+                  .then((result) => {
+                        setIsLoaded(true);
+                        setItems(result);
+                  },
+
+                        (error) => {
+                              setIsLoaded(true);
+                              setError(error);
+                        }
+                  )
+      }, [input])
+
+
+      if (error) {
+            return <div>Erreur : {error.message}</div>;
+      } else if (!isLoaded) {
+            return <div>Chargement...</div>;
+      } else {
+            if (input.length !== 0) {
+                  return (
+                        <div className="row justify-content-center m-3 p-2">
+                              <form className="mb-3">
+                                    <input
+                                          className="form-control"
+                                          type="number"
+                                          placeholder="Rechercher un personnage"
+                                          aria-label="Search"
+                                          // value={input}
+                                          onInput={e => setInput(e.target.value)}
+                                    />
+                              </form>
+                              {
+                                    Object.keys(items).map((item, i) => (
+                                          <ul className="m-2" key={i}>
+                                                <li><b>{item}</b> : {items[item]}</li>
+                                          </ul>
+                                    ))
+                              }
+                        </div>
+                  )
+            }
+
+            else {
+                  return (
+
+                        <div className="row justify-content-center m-3 p-2">
+
+                              <form className="mb-3">
+                                    <input
+                                          className="form-control"
+                                          type="number"
+                                          placeholder="Rechercher un personnage"
+                                          aria-label="Search"
+                                          // value={input}
+                                          onInput={e => setInput(e.target.value)}
+                                    />
+                              </form>
+
+                              {
+                                    setInput('1')
+                              }
+
+
+                        </div>
+
+                  )
+            }
+
+      }
+}
+
+export default Recherche;
